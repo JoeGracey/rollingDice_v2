917ad9184647194a5c25240e62ed5d37c0a78165
Joe Gracey <joe.gracey@gmail.com>
2020-12-18 20:12:19 -0700

    DiceGame

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,57 @@
 public class Main {
 
     public static void main(String[] args) {
-        test();
+        RollTheDice rollTheDice = new RollTheDice(); // Create the RollTheDice Object
+
+        rollTheDice.rolling();
+        rollTheDice.storingNumberRolled();
+        rollTheDice.table();
     }
 
-    public static void test() {
-        System.out.println("TEST");
+    public static class RollTheDice {
+        public int d20Dice;
+        int[] diceNumberFrequency = new int[21]; // 20 sided die adding 1 everytime a num is rolled
+
+        // Rolls the dice
+        public void rolling() {
+            d20Dice = (int)(Math.random() * 20) + 1;
+            System.out.println("Number rolled: " + d20Dice);
+        }
+
+        // Adds one to the index each time a number is rolled
+        public void storingNumberRolled() {
+            for (int count = 1; count <= 20; count++) {
+                ++diceNumberFrequency[d20Dice];
+                System.out.println(d20Dice + " has been rolled " + diceNumberFrequency[d20Dice]
+                 + " times");
+                rolling();
+            }
+        }
+
+        // Table showing each number rolled and how many times it was rolled
+        public void table() {
+            System.out.println("Number\tTimes Rolled");
+            System.out.println(d20Dice + "\t\t" + diceNumberFrequency[d20Dice]);
+        }
     }
+
 }
+
+// Create a dice object
+// Create an array to store each number rolled
+// Stop when 100 numbers have been added
+
+//int diceRollCount = 0; // Number of times the dice have been rolled
+
+// create an array that stores every number rolled
+
+
+//        do {
+//            // roll the dice
+//            // print number that gets rolled
+//            // add one to the number count
+//            // print the diceNumberFrequency
+//        } while (diceRollCount <= 100) {
+//            // keep rolling it again
+//            // and adding to the total
+//        }
