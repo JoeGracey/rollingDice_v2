d45ec012d13c632e1a9c4cef215a0befbfb9ab57
Joe Gracey <joe.gracey@gmail.com>
2020-12-19 23:54:22 -0700

    final

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        RollTheDice rollTheDice = new RollTheDice(); // Create the RollTheDice Object
+        Dice dice = new Dice();
 
         int count = 1;
-        if (count <= 20) {
-            do {
-                rollTheDice.rolling();
-                rollTheDice.storingNumberRolled();
-            } while (count <= 20); {
-                count++;
-            }
-            rollTheDice.table();
+        do {
+            dice.rollTheDice();
+            dice.storingNumberRolled();
+            count++;
+        } while (count <= 20); {
+            dice.table();
         }
     }
 
-    public static class RollTheDice {
-        public int d20Dice;
-        int[] diceNumberFrequency = new int[21]; // 20 sided die adding 1 everytime a num is rolled
+    public static class Dice {
+        int d20Dice;
+        int[] diceNumberFrequency = new int[21];
 
-        // Rolls the dice
-        public void rolling() {
+        public void rollTheDice() {
             d20Dice = (int)(Math.random() * 20) + 1;
             System.out.println("Number rolled: " + d20Dice);
         }
@@ -28,34 +25,17 @@ public class Main {
         // Adds one to the index each time a number is rolled
         public void storingNumberRolled() {
             ++diceNumberFrequency[d20Dice];
-            System.out.println(d20Dice + " has been rolled " + diceNumberFrequency[d20Dice] + " times");
-            rolling();
+            System.out.println(d20Dice + " has been rolled " + diceNumberFrequency[d20Dice] + " " +
+                    "time(s)");
         }
 
         // Table showing each number rolled and how many times it was rolled
         public void table() {
+            int index = 1;
             System.out.println("Number\tTimes Rolled");
-            System.out.println(d20Dice + "\t\t" + diceNumberFrequency[d20Dice]);
+            for (int count = 1; count < diceNumberFrequency.length; count++) {
+                System.out.println(index++ + "\t\t" + diceNumberFrequency[count]);
+            }
         }
     }
-
 }
-
-// Create a dice object
-// Create an array to store each number rolled
-// Stop when 100 numbers have been added
-
-//int diceRollCount = 0; // Number of times the dice have been rolled
-
-// create an array that stores every number rolled
-
-
-//        do {
-//            // roll the dice
-//            // print number that gets rolled
-//            // add one to the number count
-//            // print the diceNumberFrequency
-//        } while (diceRollCount <= 100) {
-//            // keep rolling it again
-//            // and adding to the total
-//        }
